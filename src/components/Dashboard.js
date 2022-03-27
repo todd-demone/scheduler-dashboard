3f8ca57b394789fe6fc66cfd6f8a6f76ebc84567
Todd Demone <todd.demone.dev@gmail.com>
2022-03-27 11:07:15 -0400

    create Loading component; import into Dashboard and render it when state is loading

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
-
+import Loading from "./Loading";
 import classnames from "classnames";
 
 class Dashboard extends Component {
+  // We will show a Loading component when app is loading data
+  // set up the initial loading state
+  state = {
+    loading: false,
+  };
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
+    // Show the Loading component when the state is loading
+    if (this.state.loading) {
+      return <Loading />;
+    }
     return <main className={dashboardClasses} />;
   }
 }
