9f72dc638a306dd57edbc6a756ae34aea7dcb750
Todd Demone <todd.demone.dev@gmail.com>
2022-03-27 11:19:21 -0400

    create Panel; add 4 panels to Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,30 @@
 import React, { Component } from "react";
 import Loading from "./Loading";
+import Panel from "./Panel";
 import classnames from "classnames";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6,
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm",
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday",
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3",
+  },
+];
 
 class Dashboard extends Component {
   // We will show a Loading component when app is loading data
@@ -16,7 +40,18 @@ class Dashboard extends Component {
     if (this.state.loading) {
       return <Loading />;
     }
-    return <main className={dashboardClasses} />;
+    return (
+      <main className={dashboardClasses}>
+        {data.map((panel) => (
+          <Panel
+            key={panel.id}
+            id={panel.id}
+            label={panel.label}
+            value={panel.value}
+          />
+        ))}
+      </main>
+    );
   }
 }
 
